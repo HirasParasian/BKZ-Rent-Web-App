9cabfaf8c77770462179719d13949461909fbfc2
HirasParasian <hirasparasian@gmail.com>
2022-03-02 13:00:25 +0700

    Update : Navbar Icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,27 @@ import React from 'react'
 import logo from '../assets/images/logo-bkz.png'
 import Profile from '../assets/images/profile.png'
 import { Link } from 'react-router-dom'
+import { BsEnvelope } from 'react-icons/bs'
 
 
 export const Navbar = () => {
     return (
         <nav className="navbar ms-auto navbar-expand-lg navbar-light bg-transparent">
             <div className="container ">
-                <Link className="navbar-brand" to="/">
-                    <img src={logo} alt="Avatar Logo"
-                        className="w-25 rounded-pill" />
-                </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <div className='d-flex'>
+                    <div>
+                        <Link className="navbar-brand" to="/">
+                            <img src={logo} alt="Avatar Logo"
+                                className="w-25 rounded-pill" />
+                        </Link>
+                    </div>
+                    <div className='align-self-center'>
+                        <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                </div>
                 <div className="center-navbar collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className=" navbar-nav ms-auto mb-lg-0">
                         <li className="nav-item nav-menu-before">
@@ -44,10 +51,12 @@ export const Navbar = () => {
                         </li>
                     </ul>
                     <form className="d-flex justify-content-center">
-                        <div className="mail">
-                            <Link className="" to="#">
-                                <i className="text-warning  fa-solid fa-envelope"></i>
-                            </Link>
+                        <div type="button" class="btn position-relative">
+                            <BsEnvelope size={30} color="#BF9270" />
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                99
+                                <span class="visually-hidden">unread messages</span>
+                            </span>
                         </div>
                         <div className="img-profile-login">
                             <Link className="" to="/profil">
