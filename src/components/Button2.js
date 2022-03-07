4622fba703769de345e4a81308f089a21d2a43dd
HirasParasian <hirasparasian@gmail.com>
2022-03-08 02:18:31 +0700

    update : Quantity Button

--- src/components/Button2.js
+++ src/components/Button2.js
@@ -1,34 +1,34 @@
 import React from "react"
 import { FaPlus, FaMinus } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
+import { setQuantity } from '../redux/actions/transaction'
 
-class Button2 extends React.Component {
-    state = {
-        count: 0
-    };
-    tambah = () => {
-        this.setState({ count: this.state.count + 1 })
-    }
-    kurang = () => {
-        if (this.state.count > 0) {
-            this.setState({ count: this.state.count - 1 })
+
+export const Button2 = ({ max, min = 1 }) => {
+    const transaction = useSelector(state => state.transaction)
+    const dispatch = useDispatch()
+    const incrementNumber = () => {
+        if (transaction.quantity < max) {
+            dispatch(setQuantity(transaction.quantity + 1))
         }
     }
-
-    render() {
-        return (
-            <div className="d-flex d-grid ms-4 justify-content-center align-item-center my-3">
-                <button disabled={this.state.count === 0} className="btn bg-brown rounded btn-circle d-flex justify-content-center align-item-center" onClick={this.kurang}>
-                    <FaMinus />
-                </button>
-                <button disabled className="btn d-flex justify-content-center align-item-center" >
-                    <h4>{this.state.count}</h4>
-                </button>
-                <button className="btn bg-brown ms-4  rounded btn-circle d-flex justify-content-center align-item-center" onClick={this.tambah}>
-                    <FaPlus />
-                </button>
-            </div>
-        );
+    const decrementNumber = () => {
+        if (transaction.quantity > min) {
+            dispatch(setQuantity(transaction.quantity - 1))
+        }
     }
+    return (
+        <div className="d-flex d-grid ms-4 justify-content-center align-item-center my-3">
+            <button className="btn bg-brown rounded btn-circle d-flex justify-content-center align-item-center" onClick={decrementNumber}>
+                <FaMinus />
+            </button>
+            <button disabled className="btn d-flex justify-content-center align-item-center" >
+                <h4>{transaction.quantity}</h4>
+            </button>
+            <button className="btn bg-brown ms-4  rounded btn-circle d-flex justify-content-center align-item-center" onClick={incrementNumber}>
+                <FaPlus />
+            </button>
+        </div>
+    )
 }
-
 export default Button2
