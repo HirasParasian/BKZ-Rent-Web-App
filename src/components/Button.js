3ee4d81eed70143389acc47f71bdf35088988937
HirasParasian <hirasparasian@gmail.com>
2022-03-09 13:55:58 +0700

    update : margin button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = ({ children, block, padding, variant = 'info', ...rest }) => {
     return (
-        < div className={block ? `d-grid ${padding ? padding : 'px-5'}` : `d-inline-block ${padding ? padding : 'px-5'}`} >
+        < div className={block ? `d-grid ${padding ? padding : 'px-5 my-2'}` : `d-inline-block ${padding ? padding : 'px-5'}`} >
             <button className={`btn btn-${variant}`} {...rest}>{children}</button>
         </div >
 
